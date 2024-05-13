a7f7c75a695a15048ba9ad5537a99b75dae5a135
HoangDSR <nvh.02021995@gmail.com>
2024-05-13 21:33:17 +0700

    Done while

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            While w = new While();
+//            w.StartDoWhile();
+//            w.StartWhile();
+//            w.StartFor();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
