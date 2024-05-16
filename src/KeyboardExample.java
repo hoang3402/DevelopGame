53ed230bc52fbac24d2e068ecb2d6701ae3ee6ac
HoangDSR <54090585+hoang3402@users.noreply.github.com>
2024-05-16 15:34:10 +0700

    Events. keyboard input (#14)

---
+++ src/KeyboardExample.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyboardExample extends JPanel {
+    public KeyboardExample() {
+        KeyListener listener = new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                System.out.println("keyPressed=" + KeyEvent.getKeyText(e.getKeyCode()));
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                System.out.println("keyReleased=" + KeyEvent.getKeyText(e.getKeyCode()));
+            }
+        };
+        addKeyListener(listener);
+        setFocusable(true);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Mini Tennis");
+        KeyboardExample keyboardExample = new KeyboardExample();
+        frame.add(keyboardExample);
+        frame.setSize(200, 200);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
