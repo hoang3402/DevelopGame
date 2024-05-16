36ecf3dc265e95ce5709cd6739258a4ee0b81481
hoangdsr <nvh.02021995@gmail.com>
2024-05-16 15:33:16 +0700

    Done Anonymous Class

--- src/KeyboardExample.java
+++ src/KeyboardExample.java
@@ -1,9 +1,24 @@
 import javax.swing.*;
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class KeyboardExample extends JPanel {
     public KeyboardExample() {
-        KeyListener listener = new MyKeyListener();
+        KeyListener listener = new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                System.out.println("keyPressed=" + KeyEvent.getKeyText(e.getKeyCode()));
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                System.out.println("keyReleased=" + KeyEvent.getKeyText(e.getKeyCode()));
+            }
+        };
         addKeyListener(listener);
         setFocusable(true);
     }
