e5f0e022c2440ae8a6c085cff1a2e7f498110a3a
hoangdsr <nvh.02021995@gmail.com>
2024-05-17 08:20:03 +0700

    Done Adding sound

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        KeyboardExample.main(args);
+        try {
+            GameLoop.main(args);
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
