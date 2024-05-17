3623888be8b7ae9eb76c4e2ae58717e42e65738b
hoangdsr <nvh.02021995@gmail.com>
2024-05-17 09:22:02 +0700

    Done Sound class

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         try {
             GameLoop.main(args);
-
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
