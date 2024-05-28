73cb27ea01e6453d31ab9c98c18ae726bcfb60a2
hoangdsr <nvh.02021995@gmail.com>
2024-05-28 21:52:56 +0700

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+
 public class Main {
+
+    final static int WIDTH = 800;
+    final static int HEIGHT = 800;
+
     public static void main(String[] args) {
+        JFrame frame = new JFrame("Tetris");
+
+        frame.setSize(WIDTH, HEIGHT);
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.pack();
 
     }
 }
