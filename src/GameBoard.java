a20408c9da41ca0ac7d22b3423e41f6cc59527bb
hoangdsr <nvh.02021995@gmail.com>
2024-05-29 19:01:02 +0700

    draw layout

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -6,11 +6,41 @@ import static java.awt.RenderingHints.VALUE_ANTIALIAS_ON;
 
 public class GameBoard extends JPanel implements Runnable {
 
+    private final static int BOARD_WIDTH = 360;
+    private final static int BOARD_HEIGHT = 600;
+    public static int LEFT_X;
+    public static int RIGHT_X;
+    public static int TOP_Y;
+    public static int BOTTOM_Y;
+
     private final Thread gameThread = new Thread(this);
 
     public GameBoard() {
+
+        LEFT_X = (Main.WIDTH - BOARD_WIDTH) / 2;
+        RIGHT_X = LEFT_X + BOARD_WIDTH;
+        TOP_Y = 50;
+        BOTTOM_Y = TOP_Y + BOARD_HEIGHT;
+
         setPreferredSize(new Dimension(Main.WIDTH, Main.HEIGHT));
         setLayout(null);
+    }
+
+    private void drawBoard(Graphics2D graphics2D) {
+        graphics2D.setColor(Color.BLACK);
+        graphics2D.drawRect(LEFT_X, TOP_Y, BOARD_WIDTH + 8, BOARD_HEIGHT + 8);
+    }
+
+    private void drawNextBlock(Graphics2D graphics2D) {
+        graphics2D.drawString("Next Block", RIGHT_X + 100, TOP_Y + 220);
+        graphics2D.setColor(Color.BLACK);
+        graphics2D.drawRect(RIGHT_X + 100, TOP_Y, 200, 200);
+    }
+
+    private void drawHoldBlock(Graphics2D graphics2D) {
+        graphics2D.drawString("Hold Block", LEFT_X - 300, TOP_Y + 220);
+        graphics2D.setColor(Color.BLACK);
+        graphics2D.drawRect(LEFT_X - 300, TOP_Y, 200, 200);
     }
 
     public void start() {
@@ -27,7 +57,9 @@ public class GameBoard extends JPanel implements Runnable {
         Graphics2D graphics2D = (Graphics2D) g;
         graphics2D.setRenderingHint(KEY_ANTIALIASING, VALUE_ANTIALIAS_ON);
 
-
+        drawBoard(graphics2D);
+        drawNextBlock(graphics2D);
+        drawHoldBlock(graphics2D);
     }
 
     @Override
